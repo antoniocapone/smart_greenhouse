d37a998ffc9fdb070e531b038fd902fdf4f44a0c
NoHandsMate <z170s.lol@gmail.com>
2024-07-15 21:40:02 +0200

    Updated datasheet of sg90 servo

--- requirements.md
+++ requirements.md
@@ -2,7 +2,7 @@
 # Components & Requirements
 
 - 1 x Servo SM-S2309S: 4.8V - 6V  (refers to "docs/datasheets/microservo.pdf")
-- 1 x Servo Antonio (?)
+- 1 x Servo Microservo SG-90: 4.8V - 6V (refers to "docs/datasheets/sg90_datasheet.pdf")
 - 1 x Soil Mosture Sensor: 3.3V to 5V (refers to "docs/datasheets/...Sensor Modul Datenblatt.pdf")
 - 3 x MiniWaterPump RUNCCI-YUN: 3V to 5V (nominal 3V / 4.5V) (0.12 A to 0.18 A) (refers to "docs/datasheets/mini_water_pump.txt")
 - 1 x DHT humidity and temp sensor: 3V - 5.5V (0.2mA to 1mA) (refers to "docs/datasheets/dht11.pdf")
---
+++ test_moisture_sensor/test_moisture/test_moisture.ino
@@ -0,0 +1,47 @@
+// Moisture Sensor Arduino Code
+
+
+//By Circuitdigest 
+
+
+
+
+#define sensorPin A0
+
+
+ 
+
+
+void setup() {
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+
+  Serial.print("Analog output: ");
+
+
+  Serial.println(readSensor());
+
+
+  delay(500);
+
+
+}
+
+//  This function returns the analog data to calling function
+
+int readSensor() {
+
+
+  int sensorValue = analogRead(sensorPin);  // Read the analog value from sensor
+
+
+  int outputValue = map(sensorValue, 0, 1023, 100, 0); // map the 10-bit data to 8-bit data
+
+
+  return outputValue;             // Return analog moisture value
+
+}
